46791b9e6d25892d111031db4721c63cc13af3c2
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-02 01:51:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ const reduceToAnyTrue = collection => {
   collection.forEach(element => {
     if (!!element) {
       condition = true;
+      break;
     } else {
       condition = false;
     }
